cd0e8709128d6129a4b9a6bba9e7ba9d944af096
Tankiso Montse <tmontse16@gmail.com>
2024-05-20 16:22:31 +0200

    finished modal form, added first media query

--- js/script.js
+++ js/script.js
@@ -1,22 +1,23 @@
-// var form = document.getElementById('contact-form');
-// function showForm() {
-//   if (form.style.display === 'none') {
-//     form.style.display = 'block';
-//   } else {
-//     form.style.display = 'none';
-//   }
-// }
-
-// document.getElementById('contact-btn').addEventListener('click', showForm);
 const modal = document.getElementById("modal-form");
 const openModal = document.getElementById("contact-btn");
 const closeModal = document.getElementById("submit-btn");
+var submissionMessage = document.getElementById("submission-message");
+const form = document.getElementById("contact-form");
 
 openModal.addEventListener("click", () => {
   modal.showModal();
   console.log("showing modal");
 });
 
-closeModal.addEventListener("click", () => {
-  modal.close();
+closeModal.addEventListener("click", (event) => {
+  if (form.checkValidity()) {
+    event.preventDefault()
+    submissionMessage.style.display = "block"
+    setTimeout(() => {
+      console.log("its working")
+      modal.close();
+    }, 4000);
+  }
+  
+  
 });
